709fe501cdd8b0cc32e7c8199f5c8fe96473d6d6
Anderson Freitas <andersonfreitas21@gmail.com>
2024-05-29 16:25:21 -0300

    first commit

---
+++ src/main/java/br/com/andersonfreitas21/locar/LocarApplication.java
@@ -0,0 +1,13 @@
+package br.com.andersonfreitas21.locar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andersonfreitas21/locar/LocarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andersonfreitas21.locar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
