f820be78050826bd8b66d23653b685f9115c2be9
Anderson Freitas <andersonfreitas21@gmail.com>
2024-06-09 15:06:08 -0300

    Create CategoriaVeiculo

--- src/main/java/br/com/andersonfreitas21/locar/model/veiculo/VeiculoEntity.java
+++ src/main/java/br/com/andersonfreitas21/locar/model/veiculo/VeiculoEntity.java
@@ -1,5 +1,6 @@
 package br.com.andersonfreitas21.locar.model.veiculo;
 
+import br.com.andersonfreitas21.locar.model.veiculo.enums.CategoriaVeiculo;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,6 +34,9 @@ public class VeiculoEntity {
     private Integer idMarca;
     @Column(name = "id_modelo", nullable = false)
     private Integer idModelo;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "cat")
+    private CategoriaVeiculo categoriaVeiculo;
     @Column(name = "created_at", nullable = false, updatable = false)
     @CreatedDate
     private Instant createdAt;
